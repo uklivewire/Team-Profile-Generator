9020c98aaca890f14c1aeb2e8ca1426f60fa939e
uklivewire <rickybryans0@gmail.com>
2023-01-31 15:19:17 +0000

    udpdates to index.js

--- index.js
+++ index.js
@@ -9,7 +9,31 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
-
+// const { create } = require("domain");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
+const teamMembers = [];
+
+const managerQuestions = [
+    {
+        type: "input",
+        message: "What is the Managers name?",
+        name: "name"
+    },
+    {
+        type: "input",
+        message: "What is the manager's employee ID?",
+        name: "id"
+    },
+    {
+        type: "input",
+        message: "What is the manager's email address?",
+        name: "email"
+    },
+    {
+        type: "input",
+        message: "Whats the manager's office number?",
+        name: "officenumber"
+    }
+];
 
